c1719c1997de7ff989a39ffdc32065cb84e8b086
Alca259 <alca259@gmail.com>
2020-04-15 15:31:50 +0200

    Agregar archivos de proyecto.

---
+++ Endpoints/BrickStructure.Endpoints.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrickStructure.Endpoints.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
